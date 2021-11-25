a6220bc274b3b1840396f0422dc74505796103d1
Ondrejmuran4691 <onko.muran@gmail.com>
2021-11-25 20:11:12 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bok = new Book();
+            bok.title = "LOTR";
+             void Book(string title)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
---
+++ Open-Lab-10.01/book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Open_Lab_10._01
+{
+    class Book
+    {
+        public string title;
+        private int pages;
+        
+    }
+ 
+}
