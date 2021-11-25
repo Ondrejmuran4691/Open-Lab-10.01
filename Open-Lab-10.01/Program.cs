4d8f4aa9036a704c1bea60d4eb9ee04eb842a706
Ondrejmuran4691 <onko.muran@gmail.com>
2021-11-25 21:28:46 +0100

    before

--- Open-Lab-10.01/Program.cs
+++ Open-Lab-10.01/Program.cs
@@ -9,12 +9,8 @@ namespace Open_Lab_10._01
     {
         static void Main(string[] args)
         {
-            Book bok = new Book();
-            bok.title = "LOTR";
-             void Book(string title)
-            {
-                Console.WriteLine(title);
-            }
+
+            
         }
     }
 }
